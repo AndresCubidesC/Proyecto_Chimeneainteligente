c223fb8bb89c0683c2314b2eb8824cd60e870104
fernandocruz01 <83664174+fernandocruz01@users.noreply.github.com>
2022-04-26 18:23:04 -0500

    Se agrego codigo para realizar conexion con MQTT

--- Software/pagina_web_flask/App.py
+++ Software/pagina_web_flask/App.py
@@ -5,9 +5,25 @@ from werkzeug.utils import secure_filename
 from datetime import date
 from datetime import datetime
 import os
+import paho.mqtt.client as mqtt
 app = Flask(__name__)
 app.secret_key = 'ingrese_un_valor_secreto_aca'
 app.config['UPLOAD_PATH'] = 'static/uploads/'
+mqttc=mqtt.Client()
+mqttc.connect("localhost",1883,60)
+mqttc.loop_start()
+
+# Create a dictionary called pins to store the pin number, name, and pin state:
+pins = {
+   4 : {'name' : 'GPIO 4', 'board' : 'esp8266', 'topic' : 'esp8266/4', 'state' : 'False'},
+   5 : {'name' : 'GPIO 5', 'board' : 'esp8266', 'topic' : 'esp8266/5', 'state' : 'False'}
+   }
+
+# Put the pin dictionary into the template data dictionary:
+templateData = {
+   'pins' : pins
+   }
+
 
 @app.route('/')
 def hello_world():
@@ -109,7 +125,35 @@ def upload():
 
 @app.route('/descargar/')
 def descargar():
-    return render_template('descargar.html')
+     # Pass the template data into the template main.html and return it to the user
+   return render_template('descargar.html', **templateData)
+
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<board>/<changePin>/<action>")
+def action(board, changePin, action):
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   devicePin = pins[changePin]['name']
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "1" and board == 'esp8266':
+      mqttc.publish(pins[changePin]['topic'],"1")
+      pins[changePin]['state'] = 'True'
+
+   if action == "0" and board == 'esp8266':
+      mqttc.publish(pins[changePin]['topic'],"0")
+      pins[changePin]['state'] = 'False'
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+
+   return render_template('descargar.html', **templateData)
+
+if __name__ == "__descargar__":
+   app.run(host='0.0.0.0', port=8181, debug=True)
+
 
 @app.route('/iniciar_sesion', methods=['GET', 'POST'])
 def iniciar_sesion():
