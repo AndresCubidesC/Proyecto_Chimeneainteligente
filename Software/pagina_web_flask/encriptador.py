533ad3ed19350537770380c62babd4756fb20903
fernandocruz01 <83664174+fernandocruz01@users.noreply.github.com>
2022-04-26 15:08:07 -0500

    creacion pagina web

---
+++ Software/pagina_web_flask/encriptador.py
@@ -0,0 +1,6 @@
+#Por temas de seguridad el algoritmo de encriptación no se incluirá en este codigo
+#ingresado la variable valor, debe encriptarse para tener una base de datos segura
+
+def encriptar(valor:str):
+    #Debe encriptar "valor" acá
+    return (valor)
